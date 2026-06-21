f4eb4d619de2da47cf832f4fb6175c064ae8353e
Adam Adrian <4.adam.adrian@gmail.com>
2026-06-21 10:47:54 +0700

    Update PlayerMenu.kt

--- app/src/main/kotlin/moe/rukamori/archivetune/ui/menu/PlayerMenu.kt
+++ app/src/main/kotlin/moe/rukamori/archivetune/ui/menu/PlayerMenu.kt
@@ -14,7 +14,6 @@ import android.media.audiofx.AudioEffect
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.DrawableRes
